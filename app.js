b24f1c13f60d0da718991e2bb80741e9cd03fb3e
Daniel Sikora <danielsikora100@gmail.com>
2022-10-16 17:14:02 +0200

    zad2

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const _ = require('lodash')
 
 testArray = [6, 3, 1, 8];
 
+
 /** zad 1 */
 function avg(items) {
     return _.mean(items)
 }
 
 console.log(avg(testArray))
+
+/** zad 2 */
+function min(items) {
+    return _.min(items)
+}
+function max(items) {
+    return _.max(items)
+}
+console.log(min(testArray))
+console.log(max(testArray))
+
